7889d1bb2d530ed0e25aa2c2770ca824f4427739
Sam Roth <s-roth@onu.edu>
2017-08-28 21:48:02 -0400

    Basically done with project 1

--- src/LexicalAnalyzer.h
+++ src/LexicalAnalyzer.h
@@ -2,9 +2,15 @@
 #define LEXICAL_ANALYZER_H
 
 #include <vector>
+#include <string>
 #include "Token.h"
 
 class LexicalAnalyzer {
+private:
+    bool isAlpha(char c);
+    bool isNumeric(char c);
+    bool isWhitespace(char c);
+    void printAtoms(std::vector<std::string>& atoms);
 public:
     void parseFile(std::vector<char>& buffer);
     Token getNextToken();
@@ -16,26 +22,83 @@ void LexicalAnalyzer::parseFile(std::vector<char>& buffer) {
     int openParenCount = 0;
     int closeParenCount = 0;
 
+    std::vector<std::string> literalAtoms;
+    std::vector<std::string> numericAtoms;
+
     while (position < buffer.size()) {
+        std::string temp; // for literal / numeric atoms
         char currentChar = buffer.at(position);
 
+        // Here we can fetch singleton tokens.
         switch (currentChar) {
             case '(':
                 openParenCount++;
-                position++;
                 break;
             case ')':
                 closeParenCount++;
-                position++;
                 break;
             default:
-                position++;
                 break;
         }
+
+        // Check if we are starting a valid literal atom (begins with A-Z)
+        if (isAlpha(currentChar)) {
+            temp.push_back(currentChar);
+
+            position++;
+            currentChar = buffer.at(position);
+
+            // Fetch the rest of the literal atom
+            while (!isWhitespace(currentChar)) {
+                if (isAlpha(currentChar) || isNumeric(currentChar)) {
+                    temp.push_back(currentChar);
+                    position++;
+                    currentChar = buffer.at(position);
+                } else {
+                    break;
+                }
+            }
+
+            literalAtoms.push_back(temp);
+            temp.clear();
+
+        } else if (isNumeric(currentChar)) {
+            temp.push_back(currentChar);
+
+            position++;
+            currentChar = buffer.at(position);
+
+            while (!isWhitespace(currentChar)) {
+                if (isAlpha(currentChar)) {
+                    // Error
+                } else if (isNumeric(currentChar)) {
+                    temp.push_back(currentChar);
+                    position++;
+                    currentChar = buffer.at(position);
+                } else {
+                    break;
+                }
+            }
+
+            numericAtoms.push_back(temp);
+            temp.clear();
+        }
+
+        position++;
     }
 
-    std::cout << "LITERAL ATOMS: " << std::endl;
-    std::cout << "NUMERIC ATOMS: " << std::endl;
+    std::cout << "LITERAL ATOMS: " << literalAtoms.size() << ", ";
+    printAtoms(literalAtoms);
+
+    int sum = 0;
+    std::cout << "NUMERIC ATOMS: " << numericAtoms.size() << ", ";
+    for (int i = 0; i < numericAtoms.size(); i++) {
+        std::string::size_type sz;
+        int numeric = std::stoi (numericAtoms.at(i), &sz);
+        sum += numeric;
+    }
+    std::cout << std::to_string(sum) << std::endl;
+    
     std::cout << "OPEN PARENTHESES: " << openParenCount << std::endl;
     std::cout << "CLOSING PARENTHESES: " << closeParenCount << std::endl;
 }
@@ -43,6 +106,35 @@ void LexicalAnalyzer::parseFile(std::vector<char>& buffer) {
 Token LexicalAnalyzer::getNextToken() {
     Token testToken = closingParenthesis;
     return testToken;
+}
+
+void LexicalAnalyzer::printAtoms(std::vector<std::string>& atoms) {
+    for (int i = 0; i < atoms.size() - 1; i++) {
+        std::cout << atoms.at(i) << ", ";
+    }
+    std::cout << atoms.at(atoms.size() - 1) << std::endl;
+}
+
+bool LexicalAnalyzer::isAlpha(char c) {
+    bool lowerBound = (unsigned int) c >= 65; // A
+    bool upperBound = (unsigned int) c <= 90; // Z
+
+    return lowerBound && upperBound;
+}
+
+bool LexicalAnalyzer::isNumeric(char c) {
+    bool lowerBound = (unsigned int) c >= 48; // 0
+    bool upperBound = (unsigned int) c <= 57; // 9
+
+    return lowerBound && upperBound;
+}
+
+bool LexicalAnalyzer::isWhitespace(char c) {
+    bool s = (unsigned int) c == 32; // space
+    bool r = (unsigned int) c == 13; // return
+    bool l = (unsigned int) c == 10; // line feed
+
+    return s || r || l;
 }
 
 #endif
